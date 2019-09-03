2a45c1cee5311b83f426572eadb0ed365f7e1b24
Explainaur <843347859@qq.com>
2019-09-03 21:51:33 +0800

    confirm

--- src/inst_rom.v
+++ src/inst_rom.v
@@ -5,17 +5,16 @@ module inst_rom(
 );
     
     // Define an inst array
-    reg[`InstBus] inst_mem[0:`InstMemNum - 1];
+    reg[`InstBus] inst_mem[0:`InstMemNum - 1] = 32'h34011100;
 
     // Use the file "inst_rom.data" to init the inst_rom
-    initial $readmemh ("inst_rom.data", inst_mem);
+    //initial $readmemh ("./inst_rom.data", inst_mem);
 
     always @ (*) begin
         if(ce == `ChipDisable) begin
             inst    <=    `ZeroWord;
         end else begin
-            $display("%d",inst_mem[0]);
-            inst    <=    inst_mem[addr[`InstMemNumLog2 + 1:2]];    // addr[`InstMemNumLog2+1:2] => addr/4
+            inst    <=    inst_mem[addr[`InstMemNumLog2+1:2]];    // addr[`InstMemNumLog2+1:2] => addr/4
         end
     end
 
