522051d84e492b6f0523e67730a0e80e65493d17
Explainaur <843347859@qq.com>
2019-09-08 09:55:09 +0800

    add shift test

--- mips/inst_rom.s
+++ mips/inst_rom.s
@@ -3,12 +3,28 @@
 .set noat           # free to use register $1
 
 __start:
-lui     $1, 0x0101
-ori     $1, $1, 0x0101
-ori     $2, $1, 0x1100
-or      $1, $1, $2
-andi    $3, $1, 0x00fe
-and     $1, $3, $1
-xori    $4, $1, 0xff00
-xor     $1, $4, $1
-nor     $1, $4, $1
+
+lui     $2, 0x0404
+ori     $2, $2, 0x0404
+ori     $7, $0, 0x7
+ori     $5, $0, 0x5
+ori     $8, $0, 0x8
+sll     $2, $2, 8
+sllv    $2, $2, $7
+srl     $2, $2, 8
+srlv    $2, $2, $5
+nop
+sll     $2, $2, 19
+ssnop
+sra     $2, $2, 16
+srav    $2, $2, $8
+
+; lui     $1, 0x0101
+; ori     $1, $1, 0x0101
+; ori     $2, $1, 0x1100
+; or      $1, $1, $2
+; andi    $3, $1, 0x00fe
+; and     $1, $3, $1
+; xori    $4, $1, 0xff00
+; xor     $1, $4, $1
+; nor     $1, $4, $1
